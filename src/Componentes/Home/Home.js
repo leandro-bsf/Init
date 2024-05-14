7c28c115bb4576e536b3d18226b5ec5b630e26c8
leandro barbosa <motorola1710@gmail.com>
2024-05-14 13:46:47 -0300

    criando menu superior

--- src/Componentes/Home/Home.js
+++ src/Componentes/Home/Home.js
@@ -1,11 +1,18 @@
 import React from "react"
+import Menu_Superior from "../Menu_superior/Menu_superior"
+import './styles.css';
 
 
 export default function Home(){
    console.log('chegou na home')
     return(
        <div>
-          <span>teste1</span>
+         <Menu_Superior/>
+         <div className="Div_capa">
+               
+               <h1 > Isso é um titulo</h1>
+                <span>Isso é um subtitulo</span>
+         </div>
        </div>
    
     )
--- src/Componentes/Menu_superior/Menu_superior.js
+++ src/Componentes/Menu_superior/Menu_superior.js
@@ -1,34 +1,50 @@
-import Container from 'react-bootstrap/Container';
-import Nav from 'react-bootstrap/Nav';
-import Navbar from 'react-bootstrap/Navbar';
-import NavDropdown from 'react-bootstrap/NavDropdown';
+import React, {useState, useRef} from 'react';
+import { FiLogOut } from 'react-icons/fi';
+import './styles.css';
+import  logo from './images/Logo Fundo Transparente - Escrita Branca.png'
 
-function Menu_superior() {
+export  default function Menu_Superior ({ scrollToServicos, scrollToContato, scrollToSobreNos, scrollToGaleria }){ 
+
+
+
+const [classOn, setClassOn] = useState(false);
+    
   return (
-    <Navbar expand="lg" className="bg-body-tertiary">
-      <Container>
-        <Navbar.Brand href="#home">React-Bootstrap</Navbar.Brand>
-        <Navbar.Toggle aria-controls="basic-navbar-nav" />
-        <Navbar.Collapse id="basic-navbar-nav">
-          <Nav className="me-auto">
-            <Nav.Link href="#home">Home</Nav.Link>
-            <Nav.Link href="#link">Link</Nav.Link>
-            <NavDropdown title="Dropdown" id="basic-nav-dropdown">
-              <NavDropdown.Item href="#action/3.1">Action</NavDropdown.Item>
-              <NavDropdown.Item href="#action/3.2">
-                Another action
-              </NavDropdown.Item>
-              <NavDropdown.Item href="#action/3.3">Something</NavDropdown.Item>
-              <NavDropdown.Divider />
-              <NavDropdown.Item href="#action/3.4">
-                Separated link
-              </NavDropdown.Item>
-            </NavDropdown>
-          </Nav>
-        </Navbar.Collapse>
-      </Container>
-    </Navbar>
-  );
-}
+    <header>
+    <div className="container">
+      <img src={logo} className='logo'></img>
 
-export default Menu_superior;
+      <div className = { classOn ? 'menu-section on' : 'menu-section'} onClick={() => setClassOn(!classOn)}>
+           <div className="menu-toggle">
+              <div className="one"></div>
+              <div className="two"></div>
+              <div className="three"></div>
+           </div>
+
+           <nav>
+            <ul>
+              <li>
+                <a href="/" onClick={scrollToServicos} >SERVIÇOS</a>
+              </li>
+              <li>
+                <a href="/" onClick={scrollToSobreNos}  >SOBRE</a>
+              </li>
+              <li>
+                <a href="/"onClick={scrollToGaleria} >GALERIA</a>
+              </li>
+          
+              <li>
+                <a href="/"onClick={scrollToContato} >CONTATO</a>  
+              </li>
+             
+            </ul>
+            
+          </nav>
+
+          </div>
+        </div>
+
+      </header>
+
+      )
+  }
