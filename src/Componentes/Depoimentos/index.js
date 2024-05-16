5b5ff2ae054fbd9ff397d4bf456527bae6e42ba2
leandro-bsf <motorola1710@gmail.com>
2024-05-15 22:06:15 -0300

    criacao  depoimentos

---
+++ src/Componentes/Depoimentos/index.js
@@ -0,0 +1,95 @@
+import React, { useState } from 'react';
+import './style.css';
+import { IoIosArrowForward } from "react-icons/io";
+import Button from 'react-bootstrap/Button';
+import perfil from './imagen/foto_perfil.png'
+import perfi2 from './imagen/perfil2.jpg'
+const dadosCards = [
+  {
+    id: 1,
+    imagem:perfil,
+    titulo: 'Título do Card 1',
+    avaliacao: '⭐⭐⭐⭐⭐', 
+    descricao: 'Com o design pronto, nosso time de programadores irá dar vida ao projeto. Usando tecnologias de ponta, sua ideia vira realidade.',
+  },
+  {
+    id: 2,
+    imagem: perfi2,
+    titulo: 'Nome do João',
+    avaliacao: '⭐⭐⭐⭐⭐', 
+    descricao: 'Com o design pronto, nosso time de programadores irá dar vida ao projeto. Usando tecnologias de ponta, sua ideia vira realidade.',
+  },
+  {
+    id: 3,
+    imagem: perfi2,
+    titulo: 'Nome do João',
+    avaliacao: '⭐⭐⭐⭐⭐', 
+    descricao: 'Com o design pronto, nosso time de programadores irá dar vida ao projeto. Usando tecnologias de ponta, sua ideia vira realidade.',
+  },
+  {
+    id: 4,
+    imagem: perfi2,
+    titulo: 'Nome do João',
+    avaliacao: '⭐⭐⭐⭐⭐', 
+    descricao: 'Com o design pronto, nosso time de programadores irá dar vida ao projeto. Usando tecnologias de ponta, sua ideia vira realidade.',
+  },
+  // Adicione mais cards aqui
+];
+
+const NUMERO_CARDS_POR_PAGINA = 3;
+
+function Depoimentos() {
+  const [paginaAtual, setPaginaAtual] = useState(0);
+
+  const handleProximo = () => {
+    const novaPagina = paginaAtual + 1;
+    if (novaPagina >= dadosCards.length / NUMERO_CARDS_POR_PAGINA) {
+      setPaginaAtual(0);
+    } else {
+      setPaginaAtual(novaPagina);
+    }
+  };
+
+  const handleAnterior = () => {
+    const novaPagina = paginaAtual - 1;
+    if (novaPagina < 0) {
+      setPaginaAtual(Math.floor(dadosCards.length / NUMERO_CARDS_POR_PAGINA));
+    } else {
+      setPaginaAtual(novaPagina);
+    }
+  };
+
+  const cardsNaPagina = dadosCards.slice(
+    paginaAtual * NUMERO_CARDS_POR_PAGINA,
+    (paginaAtual + 1) * NUMERO_CARDS_POR_PAGINA
+  );
+
+  return (
+    <div className="carousel">
+
+        
+    <Button  variant="link" onClick={handleAnterior} ><IoIosArrowForward /> </Button>
+      <div className="carousel-inner">
+        {cardsNaPagina.map((card) => (
+          <div className="card" key={card.id}>
+           
+             <img src={card.imagem}  className='image_card' alt={card.titulo} /> 
+            <h1>{card.titulo}</h1>
+            <span>Cliente</span>
+             <span>{card.avaliacao}</span>
+
+            <span>{card.descricao}</span>
+          </div>
+        ))}
+      </div>
+
+     
+
+      <Button  variant="link" onClick={handleProximo}>
+      <IoIosArrowForward />
+        </Button>
+    </div>
+  );
+}
+
+export default Depoimentos;
--- src/Componentes/Home/Home.js
+++ src/Componentes/Home/Home.js
@@ -1,13 +1,18 @@
 import React from "react"
 import Menu_Superior from "../Menu_superior/Menu_superior"
 import './styles.css';
-import foto_banner  from  './image_banner.png'
+
+import foto_banner  from  './imagens/image_banner.png'
 import Button from 'react-bootstrap/Button';
 import Card from 'react-bootstrap/Card';
 import { TiEdit } from "react-icons/ti";
 import { FaCode } from "react-icons/fa6";
 import { motion } from "framer-motion"
 import { IoIosArrowForward } from "react-icons/io";
+import TypeIt from "typeit-react";
+import banner2 from './imagens/banner2.png'
+import banner3 from './imagens/banner3.png'
+import Depoimentos from "../Depoimentos";
 export default function Home(){
    console.log('chegou na home')
     return(
@@ -34,8 +39,7 @@ export default function Home(){
                   </div>
              
          </div>
-           <  div className="Servicos"
-            >
+         <  div className="Servicos" >
 
           <h1>O que fazemos?</h1>
           <h2>Veja quais <span style={{ color: '#38b6ff' }} >serviços</span>  oferecemos e como posso ajudar você</h2>
@@ -50,9 +54,15 @@ export default function Home(){
              <TiEdit  className="icone_servicos"/>
                 <Card.Body>
                    <Card.Title className="titulo_servicos">Prototipação</Card.Title>
-                  <Card.Text className="Texto_car">
-                  Arquitetamos seu produto e desenvolvemos o conceito e comunicação visual. Por fim, é feito o design de todas as telas!
-                </Card.Text>
+
+                   <TypeIt
+        options={{
+          strings: ["  Arquitetamos seu produto e desenvolvemos o conceito e comunicação visual. Por fim, é feito o design de todas as telas!"],
+          speed: 40,
+          waitUntilVisible: true,
+        }}
+      />
+                
              </Card.Body>
           </Card>
 
@@ -60,9 +70,9 @@ export default function Home(){
             <FaCode  className="icone_servicos" />
                 <Card.Body>
                    <Card.Title className="titulo_servicos"  >Desenvolvimento</Card.Title>
-                  <Card.Text  className="Texto_car">
+                 <TypeIt className="Texto_car">
                   Com o design pronto, nosso time de programadores irá dar vida ao projeto. Usando tecnologias de ponta, sua ideia vira realidade.
-                </Card.Text>
+                </TypeIt>
              </Card.Body>
           </Card>
 
@@ -71,6 +81,40 @@ export default function Home(){
           
            <Button variant="info" className="button_servicos">Vamos Conversar <IoIosArrowForward /> </Button>
           </div> 
+
+          <div className="Div_Res_mob">
+          <h2>Criação de Sites <span style={{ color: '#38b6ff' }} >Responsivos </span>    e <span style={{ color: '#38b6ff' }} >Mobiles</span>  </h2>
+            
+            <div className="Div_infe" >        
+            <span>
+            Criação de sites mobiles e responsivos. Sites que se adaptam a qualquer resolução de tela, sem criar barra de rolagem.
+            Em 2016, o acesso à internet via dispositivos móvel (ou mobile) ultrapassou os acessos via desktop.      
+            </span>
+
+            <img src={banner2} className="imagem_banner"></img>
+       
+            </div>
+            <h2>Nossos Projetos</h2>
+            <span>Conheça nossos<span style={{ color: '#38b6ff' }} > projetos</span>       já finalizados para nossos clientes.</span>
+            <div className="Div_infe" >        
+            <span>
+            Clique no botão abaixo para conhecer os projetos já efetuados pelo nosso time  <br/>
+            <Button variant="info"  className="button_servicos">Portfólio <IoIosArrowForward /> </Button>   
+            </span>
+            <img src={banner3} className="imagem_banner"></img>
+            </div>
+
+          </div>
+          <div className="depoimentos">
+
+            
+          <h1>Depoimentos </h1>
+         
+          <h2> O que nossos <span style={{ color: '#38b6ff' }} >clientes</span>    dizem sobre nós</h2>
+
+         <Depoimentos/>
+
+          </div>
        </div>
    
     )
