bbb9cc2ce230263996af7cd2c6822726d0695049
leandro-bsf <motorola1710@gmail.com>
2024-05-30 22:00:07 -0300

    ajuste tela sobre nos

--- src/Componentes/Sobre_nos/index.js
+++ src/Componentes/Sobre_nos/index.js
@@ -3,10 +3,12 @@ import React from "react"
 import  './styles.css'
 import Menu_Superior from "../Menu_superior/Menu_superior"
 import '../Style_universal.css'
+import Col from 'react-bootstrap/Col';
+import Nav from 'react-bootstrap/Nav';
+import Row from 'react-bootstrap/Row';
 import Tab from 'react-bootstrap/Tab';
-import Tabs from 'react-bootstrap/Tabs';
 import Orcamento_mapa from "../Orcamento_mapa";
-import { GiStairsGoal } from "react-icons/gi";
+import  foto from './imagens/5278.jpg'
 
 export default function Sobre_nos(){
     return(
@@ -16,25 +18,41 @@ export default function Sobre_nos(){
              
 
                 <h2 className="Titulo">Sobre nós</h2>
-                <h3>Somos uma empresa com foco em criação de sites</h3>
+                <h3>Somos uma empresa com foco em criação 
+                  de sites</h3>
+                  <img src={foto} className="foto_sobre"></img>
+      
         </div>
-        
+         
              <div className="Div_cards">
-              
-            <Tabs defaultActiveKey="profile" id="uncontrolled-tab-example" className="mb-3">
-                <Tab eventKey="Missão"  title="Missão" className="item" >  
-                Criar soluções digitais excepcionais que impulsionam o sucesso dos nossos clientes, expandindo sua presença online, fortalecendo sua marca e alcançando novos mercados 
-              </Tab>
-
-                <Tab eventKey="Valores" title="Valores" className="item">
-              
-                  Cliente em primeiro lugar: Priorizamos a satisfação dos nossos clientes, construindo relacionamentos duradouros e de confiança, oferecendo um atendimento personalizado e ágil. Criatividade e Inovação: Buscamos constantemente novas ideias e soluções tecnológicas para superar as expectativas dos nossos clientes, mantendo-nos atualizados com as últimas tendências do mercado digital.
+            
+             <Tab.Container id="left-tabs-example" defaultActiveKey="first">
+      <Row>
+        <Col sm={3}>
+          <Nav variant="pills" className="flex-column">
+            <Nav.Item>
+              <Nav.Link eventKey="first">Missão</Nav.Link>
+            </Nav.Item>
+            <Nav.Item>
+              <Nav.Link eventKey="second">Valores</Nav.Link>
+            </Nav.Item>
+            <Nav.Item>
+              <Nav.Link eventKey="tri">Visão</Nav.Link>
+            </Nav.Item>
+          </Nav>
+        </Col>
+        <Col sm={9}>
+          <Tab.Content>
+            <Tab.Pane eventKey="first">    Criar soluções digitais excepcionais que impulsionam o sucesso dos nossos clientes, expandindo sua presença online, fortalecendo sua marca e alcançando novos mercados .</Tab.Pane>
+            <Tab.Pane eventKey="second">   Cliente em primeiro lugar: Priorizamos a satisfação dos nossos clientes, construindo relacionamentos duradouros e de confiança, oferecendo um atendimento personalizado e ágil. Criatividade e Inovação: Buscamos constantemente novas ideias e soluções tecnológicas para superar as expectativas dos nossos clientes, mantendo-nos atualizados com as últimas tendências do mercado digital.
                   Qualidade e Excelência: Nos dedicamos a entregar projetos impecáveis, com alto nível de qualidade e dentro dos prazos acordados, garantindo a total satisfação dos nossos clientes.
-                </Tab>
-
-                 
-                <Tab eventKey="Visão" title="Visão" className="item" > Ser a referência em desenvolvimento de sites e home pages, reconhecida pela excelência, inovação e impacto positivo nos negócios dos nossos clientes, tornando-se a escolha preferida de empresas que buscam crescimento e sucesso digital.</Tab>
-    </Tabs>
+               </Tab.Pane>
+            <Tab.Pane eventKey="tri"> Ser a referência em desenvolvimento de sites e home pages, reconhecida pela excelência, inovação e impacto positivo nos negócios dos nossos clientes, tornando-se a escolha preferida de empresas que buscam crescimento e sucesso digital.</Tab.Pane>
+          </Tab.Content>
+        </Col>
+        
+      </Row>
+    </Tab.Container>
              </div>
            
          <Orcamento_mapa/>
